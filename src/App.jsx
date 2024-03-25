e6e706f885d366ee785b665305ba19911d95021d
Oliver Kennelly <victoriakennelly@gmail.com>
2024-03-25 11:20:58 -0500

    inital commit

---
+++ src/App.jsx
@@ -0,0 +1,70 @@
+import { useEffect, useState } from "react"
+import { getAllTickets } from "./services/ticketService"
+import "./App.css"
+
+export const App = () => {
+  const [allTickets, setAllTickets] = useState([])
+  const [showEmergencyOnly, setShowEmergency] = useState(false)
+
+  useEffect(() => {
+    getAllTickets().then((ticketsArray) => {
+      setAllTickets(ticketsArray)
+    })
+  }, []) //ONLY runs on initial render of component
+
+  useEffect(() => {
+
+  }, [showEmergencyOnly])
+
+  return (
+  <div className="tickets-container">
+    <h2>Tickets</h2>
+    <div>
+      <button className="filter-btn btn-primary" onClick={() => {
+        setShowEmergency(true)
+      }}>Emergency</button>
+      <button className="filter-btn btn-info" onClick={() => {
+        setShowEmergency(false)
+      }}>Show All</button>
+    </div>
+    <article className="tickets">
+      {allTickets.map(ticket => {
+        return (
+          <section className="ticket" key={ticket.id}>
+            <header className="ticket-info">#{ticket.id}</header>
+            <div> {ticket.description}</div>
+            <footer>
+              <div>
+                <div className="ticket-info">emergency</div>
+                <div>{ticket.emergency ? "yes" : "no"}</div>
+              </div>
+            </footer>
+          </section>
+        )
+      })}
+    </article>
+  </div>
+  )
+}
+
+/*
+export const App = () => {
+  const [count, setCount] = useState(0) //[stateVariable, setterFunction]
+  //useState takes one argument, setting the inital value of the statedVariale. If nothing is passed, the value is undefined
+
+  const handleButtonClick = () => {
+    setCount(count + 1)
+    // this function is important because when it gets invoked, useState rerenders
+  }
+
+  return (
+  <>
+    <h1>Hello!</h1>
+    <div>This is amazing!</div>
+    <button className="btn-secondary" onClick={handleButtonClick}>Click me!</button>
+    <div>Count: {count}</div>
+  </>
+  )
+  // returns like this must have a parent. These are called react fragments
+}
+*/

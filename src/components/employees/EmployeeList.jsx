5f41b99acd2607c362c007ce69312cc17fa523e4
Oliver Kennelly <victoriakennelly@gmail.com>
2024-03-27 16:14:46 -0500

    employee list

---
+++ src/components/employees/EmployeeList.jsx
@@ -0,0 +1,19 @@
+import { useEffect, useState } from "react"
+import { getStaffUsers } from "../../services/userService"
+import { User } from "../../users/User"
+
+export const EmployeeList = () => {
+    const [employees, setEmployees] = useState([])
+
+    useEffect(() => {
+        getStaffUsers().then((employeeArray) => {
+            setEmployees(employeeArray)
+        })
+    }, [])
+
+    return <div className="employees">
+        {employees.map(employeeObj => {
+            return <User user={employeeObj} />
+        })}
+    </div>
+}
--- src/services/userService.js
+++ src/services/userService.js
@@ -3,3 +3,9 @@ export const getNonStaffUsers = async () => {
         res.json()
     )
 }
+
+export const getStaffUsers = async () => {
+    return fetch(`http://localhost:8088/users?isStaff=true`).then((res) => 
+        res.json()
+    )
+}
